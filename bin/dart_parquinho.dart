41399340e628b2763853a2043bb75c4e55bac4d8
Marcelo Macorim <marcorim@gmail.com>
2022-08-15 19:59:26 -0300

    primeira inclusão

---
+++ bin/dart_parquinho.dart
@@ -0,0 +1,5 @@
+import 'package:dart_parquinho/dart_parquinho.dart' as dart_parquinho;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_parquinho.calculate()}!');
+}
---
+++ test/dart_parquinho_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_parquinho/dart_parquinho.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
